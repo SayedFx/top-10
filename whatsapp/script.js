6684ea0dc3fd150b4b27648e01b5bdacc2a4a6f9
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-02-04 20:43:40 +0000

    finish section one and section six

---
+++ whatsapp/script.js
@@ -0,0 +1,11 @@
+const hamburgerButton = document.querySelector(".hamburger-button");
+const nav = document.querySelector(".nav");
+const crossBtn = document.querySelector(".cross-btn");
+
+hamburgerButton.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+
+crossBtn.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
