d92daa60eb8da1a1885160701ddad4aac3564f94
Yousef Sayed <sayed.yousaf@gmail.com>
2022-02-05 18:02:35 +0000

    fix responsiveness

--- whatsapp/script.js
+++ whatsapp/script.js
@@ -1,14 +1,21 @@
 const hamburgerButton = document.querySelector(".hamburger-button");
 const nav = document.querySelector(".nav");
 const crossBtn = document.querySelector(".cross-btn");
+const langBtn = document.querySelector(".lang-btn");
+const langPicker = document.querySelector(".transparent-background");
+const closeLangPicker = document.querySelector(".close-lang-picker");
+
+langBtn.addEventListener("click", () => langPicker.classList.add("visible"));
+closeLangPicker.addEventListener("click", () =>
+  langPicker.classList.remove("visible")
+);
+
+setTimeout(() => (nav.style.display = "flex"), 1000);
 
 hamburgerButton.addEventListener("click", () => {
-  nav.classList.remove("visible");
+  nav.classList.toggle("visible");
 });
 
 crossBtn.addEventListener("click", () => {
-  nav.classList.add("visible");
+  nav.classList.toggle("visible");
 });
-
-
-
